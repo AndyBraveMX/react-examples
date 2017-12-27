4550d3e7dc3b2323c66e679e975a6592b1c92928
andy bravo <anbrav@hotmail.com>
2017-12-27 17:21:19 -0600

    Retomo el curso

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({hi: 'Andres desde house'});
+	res.send({hi: 'Andres segundo'});
 });
 
 const PORT = process.env.PORT || 5000;
