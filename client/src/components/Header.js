b9605d7bb971dee56ff1c65306109b487db77f8b
andy bravo <andres.bravo@propiedades.com>
2018-03-14 17:24:35 -0600

    :newspaper: Se agrega funcionalidad para login y logout desde header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class Header extends Component {
+
+	rendercContent(){
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return <li><a href="/auth/google">Login with Google</a></li>
+			default:
+				return <li><a href="/api/logout">Logout</a></li>
+		}
+	}
+
 	render() {
 		return (
 			<nav>
@@ -9,9 +22,7 @@ class Header extends Component {
 						Emaily
 					</a>
 					<ul className="right">
-						<li>
-							<a>Login with Google</a>
-						</li>
+						{this.rendercContent()}
 					</ul>
 				</div>
 			</nav>
@@ -19,4 +30,9 @@ class Header extends Component {
 	}
 }
 
-export default Header;
+
+function mapStateToProps({auth}){
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
